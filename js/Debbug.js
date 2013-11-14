2e0fde42c6bbee52296c153bcc88174ec0e83776
ricardo cerqueira <ricardomccerqueira@gmail.com>
2013-11-14 09:37:58 +0000

    Overwritting console

--- js/Debbug.js
+++ js/Debbug.js
@@ -1,33 +1,44 @@
-var debug = (function(){
+var console = (function(){
     var active = true; // false to disable all debbug calls
+    var native = window.console;
+
+    function getStackTrace(date){
+      var obj = {};
+      Error.captureStackTrace(obj, getStackTrace);
+      return obj.stack.toString().replace('Error',date);
+    };
 
     function log(){
         if(active){
-            try{
-                console.log(arguments.callee.caller.arguments.callee.caller.name + "() called " + arguments.callee.caller.name + "() called debug.log()");
-            }catch(e){
-
-            }
-    
-            var items = [];
-            var date = new Date().toString().split(" ");
+            var date = new Date().toString().split(' ');
                 date.length = 5;
-                date = date.toString().split(",").join(" ");
+                date = '\n'+date.toString().split(',').join(' ');
 
+            native.log(getStackTrace(date));
+
+            var items = [];
 
             for (var i=0; i < arguments.length; i++) {
                 items.push(arguments[i]);
             }
 
-            console.log(date, items);
-
-
-            console.log('\n');
+            native.log('\n log:',items,'\n\n');
         }
     };
 
+    function stack(){
+         native.log(getStackTrace());
+    };
+
     var public = {
-        log:log
+        log:log,
+        stack:stack,
+        disable:function(){
+            active = false;
+        },
+        enable:function(){
+            active = true;
+        }
     }
 
     return public;
